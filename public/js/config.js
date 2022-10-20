575d240c5557402dd42217523fe1cf8169d423fb
Diego Pontes de Carvalho <diego.pontes@group.zhazspsc>
2022-10-20 11:59:37 -0300

    novo

--- public/js/config.js
+++ public/js/config.js
@@ -4,23 +4,22 @@ function createButton() {
    btn.className = "btn btn-success";
    let lbl = document.createTextNode('Entrar');
    btn.appendChild(lbl);
+   // localStorage.logado = 0;
 
    btn.onclick = function () {
-
       const usuario = document.querySelector(".input-group-text").value
       const senhas = document.querySelector(".senhas").value
 
-      const usd1 = usds.us1;
-      const sns1 = usds.sns;
-
+      
       const usd2 = 'natanael';
       const sns2 = '5532';
 
-      if (((usuario === usd1) && (senhas === sns1)) || ((usuario === usd2) && (senhas === sns2))) {
-         window.location.href = 'http://localhost:5000/home'
+      if (((usuario === usds.us1) && (senhas === usds.sns)) || ((usuario === usd2) && (senhas === sns2))) {
+         window.location.href = 'http://192.168.15.80:5000/home'
+         localStorage.logado = 1;
       } else {
          alert(`Usuario ou Senha Invalida!!! Verifique as inforções e veja se estão corretas`)
-         
+         localStorage.logado = 0;
       }
    }
 
